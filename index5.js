373f573545c18ff2ec0de95a9f538f88d4a82567
kostyamalashuk <105971197+kostyan289@users.noreply.github.com>
2022-09-17 13:36:30 +0300

    Add files via upload

---
+++ index5.js
@@ -0,0 +1,16 @@
+const Str = prompt('Введите слово или предложение');
+console.log(Str)
+console.log(vowel_count(Str));
+alert(`количество русских гласных букв в вашем предложении = ${vowel_count(Str)}`)
+
+function vowel_count(str1) {
+    const vowel_list = 'ауоиэыяюеёАУОИЭЫЯЮЕЁ';
+    let vcount = 0;
+
+    for(let x = 0; x < str1.length ; x++) {
+      if (vowel_list.indexOf(str1[x]) !== -1) {
+        vcount += 1;
+      }
+    }
+    return vcount;
+  }
